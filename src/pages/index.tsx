4c0edbd4873f8447831465960dfcec7d3d4ca5ed
Roger Esteve <rogeresteve30@gmail.com>
2023-05-29 21:41:21 +0200

    styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import Button from "react-bootstrap/Button";
+import {
+    Container,
+    Row,
+    Col
+} from "react-bootstrap";
+
+
+import { useState } from "react";
 
 export default function Home() {
+    
+    const [list, setList] = useState(['01/06 23:59 - Lab 6 (Xarxes)', 'tardes']); 
+
     return (
         <>
-            <Button>Buenas</Button>
+            <Container>
+                {
+                    list.map((item: string, index: number) => (
+                        <Row>
+                            <Col>
+                                <span>{item}</span>
+                            </Col>
+                        </Row>
+                    ))
+                }
+            </Container>
         </>
     )
 }
